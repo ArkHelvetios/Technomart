a93bcd622457546cde3cd60dff42f9432a77d118
ArkHelvetios <arkhelvetios@gmail.com>
2022-10-13 21:44:15 +0300

    Modals for both pages (Final - Level-1)
    Re do JS structure by using if().
    All work on both pages by now.
    Re do readme.

--- Readme.md
+++ Readme.md
@@ -1,43 +1,25 @@
-# Личный проект «Техномарт»
+# Student project «Technomart» – HTML Academy Level-1
 
-* Студент: [Александр Гельвет](https://htmlacademy.ru/profile/id1944067).
-* Наставник: `Discord/Solo`.
-
----
-
-_Не удаляйте и не обращайте внимание на файлы:_<br>
-_`.editorconfig`, `.gitattributes`, `.gitignore`, `Contributing.md`, `Readme.md`._
+* Student: [Александр Гельвет](https://htmlacademy.ru/profile/id1944067).
+* Mentor: `None/Discord`.
 
 ---
 
-### Памятка
+## Educational goals
 
-#### 1. Зарегистрируйтесь на Гитхабе
-
-Если у вас ещё нет аккаунта на [github.com](https://github.com/join), скорее зарегистрируйтесь.
-
-#### 2. Создайте форк
-
-[Откройте мастер-репозиторий]({{homepage}}) и нажмите кнопку «Fork» в правом верхнем углу. Репозиторий из Академии скопируется в ваш аккаунт.
-
-<img width="769" alt="" src="https://user-images.githubusercontent.com/10909/29037784-cf833fec-7bad-11e7-8eec-dfe32aac11b1.jpg">
-
-Получится вот так:
-
-<img width="769" alt="" src="https://user-images.githubusercontent.com/10909/29037785-d1363f60-7bad-11e7-99da-f02a2f996a01.jpg">
-
-#### 3. Клонируйте репозиторий на свой компьютер
-
-Будьте внимательны: нужно клонировать свой репозиторий (форк), а не репозиторий Академии. Нажмите кнопку «Clone or download», а затем «Open in Desktop», чтобы клонировать репозиторий через программу [GitHub Desktop](https://desktop.github.com):
-
-<img width="769" alt="" src="https://user-images.githubusercontent.com/10909/29037788-d26a3558-7bad-11e7-9d08-2f9f0f6e467a.jpg">
-
-Программа клонирует репозиторий на ваш компьютер и подготовит всё необходимое для старта работы.
-
-#### 4. Начинайте обучение!
+1. GitHub basics (no console).
+2. Figma basics.
+3. Semantically true markup.
+4. HTML specification, markup validation.
+5. Content and decorative graphic styles.
+6. CSS specification, cascade, inheritance and specificity.
+7. Grid and Flexbox layouts.
+8. Micro-layouts for components, content overflow.
+9. Accessability, objects state, screenreaders.
+10. JavaScript basics (open/close modals).
 
 ---
 
 <a href="https://htmlacademy.ru/intensive/htmlcss"><img align="left" width="50" height="50" alt="HTML Academy" src="https://up.htmlacademy.ru/static/img/intensive/htmlcss/logo-for-github-2.png"></a>
 
-Репозиторий создан для обучения на профессиональном онлайн‑курсе «[HTML и CSS. Профессиональная вёрстка сайтов](https://htmlacademy.ru/intensive/htmlcss)» от [HTML Academy](https://htmlacademy.ru).
+This repository was created for education on professional online-course «[HTML и CSS. Level-1](https://htmlacademy.ru/intensive/htmlcss)» by [HTML Academy](https://htmlacademy.ru).
--- js/modal.js
+++ js/modal.js
@@ -1,74 +1,102 @@
-/* Элементы модальной формы */
-const formButton = document.querySelector(".link-modal-form")
+/* MODAL FORM */
+const formButton = document.querySelector(".link-modal-form");
 const formModal = document.querySelector(".modal-form");
-const formModalContent = formModal.querySelector(".modal-form__wrapper");
-const formModalNameInput = formModalContent.querySelector("[name=modal-form_name]");
-const formModalEmailInput = formModalContent.querySelector("[name=modal-form_email]");
-const formModalTextarea = formModalContent.querySelector("[name=modal-form_message]");
 
-/* Элементы модальной карты */
+if (formModal) {
+  const formModalContent = formModal.querySelector(".modal-form__wrapper");
+  const formModalNameInput = formModalContent.querySelector("[name=modal-form_name]");
+  const formModalEmailInput = formModalContent.querySelector("[name=modal-form_email]");
+  const formModalTextarea = formModalContent.querySelector("[name=modal-form_message]");
+
+  /* Проверяем поддержку localStorage */
+  let isLocalStorage = true;
+  let storageName = "";
+  let storageEmail = "";
+
+  try {
+    storageName = localStorage.getItem("name");
+    storageEmail = localStorage.getItem("email");
+  } catch (err) {
+    isLocalStorage = false;
+  }
+
+  /* Кнопка открытия формы */
+  formButton.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    formModal.classList.add("modal_active");
+
+    /* Проверяем и подставляем localStorage */
+    if (storageName && !storageEmail) {
+      formModalNameInput.value = storageName;
+      formModalEmailInput.focus();
+    } else if (storageName && storageEmail) {
+      formModalNameInput.value = storageName;
+      formModalEmailInput.value = storageEmail;
+      formModalTextarea.focus();
+    } else {
+      formModalNameInput.focus();
+    }
+  });
+
+  /* Проверка на заполнение required полей и запись в localStorage */
+  formModalContent.addEventListener("submit", (evt) => {
+    if (!formModalEmailInput.value || !formModalTextarea.value) {
+      evt.preventDefault();
+      formModal.classList.remove("modal_error");
+      formModal.offsetWidth = formModal.offsetWidth;
+      formModal.classList.add("modal_error");
+    } else {
+      if (isLocalStorage) {
+        localStorage.setItem("name", formModalNameInput.value);
+        localStorage.setItem("email", formModalEmailInput.value);
+      }
+    }
+  });
+}
+
+/* MODAL MAP */
 const mapButton = document.querySelector(".link-modal-map");
 const mapModal = document.querySelector(".modal-map");
 
-/* Элементы модального окна сообщения корзины */
-const addToCartButtons = document.querySelectorAll(".products__buy-button");
-const addToCartModal = document.querySelector(".modal-add-to-cart");
-
-/* Все кнопки закрытия окон */
-const closeModalButtons = document.querySelectorAll(".modal__close-button");
-
-/* Проверяем поддержку localStorage */
-let isLocalStorage = true;
-let storageName = "";
-let storageEmail = ""
-
-try {
-  storageName = localStorage.getItem("name");
-  storageEmail = localStorage.getItem("email");
-} catch (err) {
-  isLocalStorage = false;
-}
-
-/* Кнопка открытия формы */
-formButton.addEventListener("click", (evt) => {
-  evt.preventDefault();
-  formModal.classList.add("modal_active");
-
-  /* Проверяем и подставляем localStorage */
-  if (storageName && !storageEmail) {
-    formModalNameInput.value = storageName;
-    formModalEmailInput.focus();
-  } else if (storageName && storageEmail) {
-    formModalNameInput.value = storageName;
-    formModalEmailInput.value = storageEmail;
-    formModalTextarea.focus();
-  } else {
-    formModalNameInput.focus();
-  }
-});
-
-/* Кнопка открытия карты */
-mapButton.addEventListener("click", (evt) => {
+if (mapModal) {
+  /* Кнопка открытия карты */
+  mapButton.addEventListener("click", (evt) => {
   evt.preventDefault();
   mapModal.classList.add("modal_active");
 });
-
-/* Кнопки добавления в корзину */
-for (let addToCartButton of addToCartButtons) {
-  addToCartButton.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    addToCartModal.classList.add("modal_active");
-  });
 }
+
+/* MODAL ADD TO CART */
+const addToCartButtons = document.querySelectorAll(".products__buy-button");
+const addToCartModal = document.querySelector(".modal-add-to-cart");
+
+if (addToCartModal) {
+  /* Кнопки добавления в корзину */
+  for (let addToCartButton of addToCartButtons) {
+    addToCartButton.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      addToCartModal.classList.add("modal_active");
+    });
+  }
+}
+
+/* MODAL CLOSE */
+const closeModalButtons = document.querySelectorAll(".modal__close-button");
 
 /* Кнопки закрытия модальных окон */
 for (let closeModalButton of closeModalButtons) {
   closeModalButton.addEventListener("click", (evt) => {
     evt.preventDefault();
-    formModal.classList.remove("modal_active");
-    formModal.classList.remove("modal_error");
-    mapModal.classList.remove("modal_active");
-    addToCartModal.classList.remove("modal_active");
+    if (formModal) {
+      formModal.classList.remove("modal_active");
+      formModal.classList.remove("modal_error");
+    };
+    if (mapModal) {
+      mapModal.classList.remove("modal_active");
+    }
+    if (addToCartModal) {
+      addToCartModal.classList.remove("modal_active");
+    }
   })
 }
 
@@ -76,24 +104,15 @@ for (let closeModalButton of closeModalButtons) {
 window.addEventListener("keydown", (evt) => {
   if (evt.key === "Esc" || evt.key === "Escape") {
     evt.preventDefault();
-    formModal.classList.remove("modal_active");
-    formModal.classList.remove("modal_error");
-    mapModal.classList.remove("modal_active");
-    addToCartModal.classList.remove("modal_active");
-  }
-})
-
-/* Проверка на заполнение required полей и запись в localStorage */
-formModalContent.addEventListener("submit", (evt) => {
-  if (!formModalEmailInput.value || !formModalTextarea.value) {
-    evt.preventDefault();
-    formModal.classList.remove("modal_error");
-    formModal.offsetWidth = formModal.offsetWidth;
-    formModal.classList.add("modal_error");
-  } else {
-    if (isLocalStorage) {
-      localStorage.setItem("name", formModalNameInput.value);
-      localStorage.setItem("email", formModalEmailInput.value);
+    if (formModal) {
+      formModal.classList.remove("modal_active");
+      formModal.classList.remove("modal_error");
+    };
+    if (mapModal) {
+      mapModal.classList.remove("modal_active");
+    }
+    if (addToCartModal) {
+      addToCartModal.classList.remove("modal_active");
     }
   }
 })
